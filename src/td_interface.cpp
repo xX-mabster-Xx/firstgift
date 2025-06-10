98d7e88ec7ce4bbc885a30724fb3ffe1aeaa476a
xX-mabster-Xx <s22d_usolcev@179.ru>
2025-06-10 12:40:55 +0300

    eblan

--- src/td_interface.cpp
+++ src/td_interface.cpp
@@ -365,7 +365,7 @@ void TdInterface::upgrade_loop(int millis)
                 break;
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(25));
+        std::this_thread::sleep_for(std::chrono::milliseconds(millis));
     }
 }
 
